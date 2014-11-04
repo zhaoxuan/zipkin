3592ee5de729d5f70d8a9ff05cb76c5a5bdb649b
zhaoxuan <zhaoxuan1727@gmail.com>
2014-11-04 16:34:25 +0800

    implement kafka service

---
+++ zipkin-kafka/src/main/scala/com/twitter/zipkin/kafka/KafkaService.scala
@@ -0,0 +1,27 @@
+package com.twitter.zipkin.kafka
+
+import com.twitter.finagle.Service
+import com.twitter.util.Future
+import com.twitter.zipkin.common.Span
+import kafka.message.Message
+import kafka.producer.{ProducerData, Producer}
+
+/**
+ * Created by john on 11/4/14.
+ */
+
+class KafkaService(
+                    kafka: Producer[String, String],
+                    topic: String
+                    ) extends Service[Span, Unit] {
+  def apply(data: Span): Future[Unit] = {
+    val producerData = new ProducerData[String, String](topic, Seq("123"))
+
+//    Future {
+//      kafka.send(m)
+//    } onSuccess { (_) =>
+//      println("send to kafka success")
+//    }
+    Future(Unit)
+  }
+}
