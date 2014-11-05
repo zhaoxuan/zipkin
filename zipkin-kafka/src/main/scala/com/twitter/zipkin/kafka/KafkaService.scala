e0cf64c7199c771821dd47a049b09853d3a05bbf
zhaoxuan <zhaoxuan1727@gmail.com>
2014-11-05 13:07:30 +0800

    use kafak to send span data

--- zipkin-kafka/src/main/scala/com/twitter/zipkin/kafka/KafkaService.scala
+++ zipkin-kafka/src/main/scala/com/twitter/zipkin/kafka/KafkaService.scala
@@ -3,8 +3,9 @@ package com.twitter.zipkin.kafka
 import com.twitter.finagle.Service
 import com.twitter.util.Future
 import com.twitter.zipkin.common.Span
-import kafka.message.Message
-import kafka.producer.{ProducerData, Producer}
+import kafka.producer.KeyedMessage
+import kafka.javaapi.producer.Producer
+
 
 /**
  * Created by john on 11/4/14.
@@ -16,13 +17,19 @@ class KafkaService(
                     ) extends Service[Span, Unit] {
 
   def apply(data: Span): Future[Unit] = {
-    val producerData = new ProducerData[String, String](topic, Seq("123"))
+    val msg = spanFormat(data)
+    val keyMsg = new KeyedMessage[String, String](topic, msg)
 
-//    Future {
-//      kafka.send(m)
-//    } onSuccess { (_) =>
-//      println("send to kafka success")
-//    }
-    Future.Unit
+    Future {
+      kafka.send(keyMsg)
+    } onSuccess { (_) =>
+      println("send to kafka success")
+    }
+
+  }
+
+  def spanFormat(span: Span): String = {
+    val data: String = "test data"
+    data
   }
 }
